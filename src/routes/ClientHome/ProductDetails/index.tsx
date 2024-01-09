01834af4c2e5f5b80b7b8514fa17dc868c666860
Wellington <rodrigueswellington3@gmail.com>
2024-01-09 11:37:20 -0300

    Clear Cart, Increase/decrease cart items

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -4,11 +4,11 @@ import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
 import { ProductDTO } from "../../../models/product";
+import * as cartService from "../../../services/cart-service";
 import * as productService from "../../../services/product-service";
 import "./styles.css";
 
 export default function ProductDetails() {
-
   const params = useParams();
 
   const navigate = useNavigate();
@@ -16,7 +16,8 @@ export default function ProductDetails() {
   const [product, setProduct] = useState<ProductDTO>();
 
   useEffect(() => {
-    productService.findById(Number(params.productId))
+    productService
+      .findById(Number(params.productId))
       .then((response) => {
         console.log(response.data);
         setProduct(response.data);
@@ -26,12 +27,23 @@ export default function ProductDetails() {
       });
   }, []);
 
+  function handleBuyClick() {
+    if (product) {
+      console.log(product);
+      cartService.addProduct(product);
+      navigate("/cart");
+    }
+  }
+
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
         {product && <ProductDetailsCard product={product} />}
         <div className="dsc-btn-page-container">
-          <ButtonPrimary text="Comprar" />
+          <div onClick={handleBuyClick}>
+            <ButtonPrimary text="Comprar" />
+          </div>
+
           <Link to="/">
             <ButtonInverse text="Início" />
           </Link>
