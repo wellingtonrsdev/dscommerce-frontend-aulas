079ce00b72ae7715c9c990853452c5b76e0f06cf
Wellington <rodrigueswellington3@gmail.com>
2024-01-11 00:48:59 -0300

    Catalog next page

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,8 +3,15 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+  ],
+  overrides: [
+    {
+      files: ['*.ts'],
+      extends: ['plugin:@typescript-eslint/disable-type-checked'],
+    },
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -6,19 +6,39 @@ import { ProductDTO } from "../../../models/product";
 import * as productService from "../../../services/product-service";
 import "./styles.css";
 
+type QueryParams = {
+  page: number;
+  name: string;
+};
+
 export default function Catalog() {
+
+  const[isLastPage, setIsLastPage] = useState(false);
+
   const [products, setProducts] = useState<ProductDTO[]>([]);
 
-  const [productName, setProductName] = useState("");
+  const [queryParams, setQueryParams] = useState<QueryParams>({
+    page: 0,
+    name: "",
+  });
 
   useEffect(() => {
-    productService.findPageRequest(0, productName).then((response) => {
-      setProducts(response.data.content);
-    });
-  }, [productName]);
+    productService
+      .findPageRequest(queryParams.page, queryParams.name)
+      .then((response) => {
+        const nextPage = response.data.content;
+        setProducts(products.concat(nextPage));
+        setIsLastPage(response.data.last);
+      });
+  }, [queryParams]);
 
   function handleSearch(searchText: string) {
-    setProductName(searchText)
+    setProducts([]);
+    setQueryParams({ ...queryParams, page: 0, name: searchText });
+  }
+
+  function handleNextPageClick() {
+    setQueryParams({ ...queryParams, page: queryParams.page + 1 });
   }
 
   return (
@@ -32,7 +52,12 @@ export default function Catalog() {
           ))}
         </div>
 
-        <ButtonNextPage />
+        {
+          !isLastPage &&
+          <div onClick={handleNextPageClick}>
+            <ButtonNextPage />
+          </div>
+        }
       </section>
     </main>
   );
