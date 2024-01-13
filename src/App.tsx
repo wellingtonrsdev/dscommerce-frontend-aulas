68d12e53398456c349cca050fba429f0ef358594
Wellington <rodrigueswellington3@gmail.com>
2024-01-13 11:51:15 -0300

    Layout Login

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,27 @@ import Cart from "./routes/ClientHome/Cart";
 import Catalog from "./routes/ClientHome/Catalog";
 import ProductDetails from "./routes/ClientHome/ProductDetails";
 import { ContextCartCount } from "./utils/context-cart";
+import Login from "./routes/ClientHome/Login";
 
 export default function App() {
 
   const [contextCartCount, setContextCartCount] = useState<number>(0);
 
   return (
-    <ContextCartCount.Provider value={{contextCartCount, setContextCartCount}}>
+    <ContextCartCount.Provider
+      value={{ contextCartCount, setContextCartCount }}
+    >
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<ClientHome />}>
             <Route index element={<Catalog />} />
             <Route path="catalog" element={<Catalog />} />
-            <Route path="product-details/:productId" element={<ProductDetails />} />
+            <Route
+              path="product-details/:productId"
+              element={<ProductDetails />}
+            />
             <Route path="cart" element={<Cart />} />
+            <Route path="login" element={<Login />} />
           </Route>
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
