15760fa7bd9558330e8e18a371852968fd748007
Wellington <rodrigueswellington3@gmail.com>
2024-01-23 12:48:21 -0300

    Forms validate

--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -19,3 +19,14 @@ export function updateAll(inputs: any, newValues: any) {
   }
   return newInputs;
 }
+
+export function validate(inputs: any, name: string) {
+
+  if (!inputs[name].validation) {
+    return inputs;
+  }
+
+  const isInValid = !inputs[name].validation(inputs[name].value);
+
+  return { ...inputs, [name]: { ...inputs[name], invalid: isInValid.toString()}};
+}
