879af841163a3e0c2cad6637108307b9ead5f205
Wellington <rodrigueswellington3@gmail.com>
2024-02-14 23:51:22 -0300

    Deploy ready

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+window.React = React;
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
